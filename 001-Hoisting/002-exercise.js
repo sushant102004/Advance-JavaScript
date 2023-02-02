9f815f49b2e6d71c537a40f27a6ce1177d75c85d
sushant102004 <sushant.dhiman9812@gmail.com>
2023-02-02 23:47:17 +0530

    Hoisting Exercise

---
+++ 001-Hoisting/002-exercise.js
@@ -0,0 +1,9 @@
+var favFood = 'grapes'
+
+var foodThoughts = function () {
+    console.log('Original favourite food: ' + favFood)
+    var favFood = 'apple'
+    console.log('New favourite food: ' + favFood)
+}
+
+foodThoughts()
