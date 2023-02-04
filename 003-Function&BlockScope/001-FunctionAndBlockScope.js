c4fbf99f61f1a53676eb7ef0355813b4fc54c4ec
sushant102004 <sushant.dhiman9812@gmail.com>
2023-02-04 22:58:32 +0530

    Minor Changes

--- 003-Function&BlockScope/001-FunctionAndBlockScope.js
+++ 003-Function&BlockScope/001-FunctionAndBlockScope.js
@@ -1,7 +1,7 @@
 /*
 JavaScript has function scope. 
 Variables declared inside function can't be accessed outside.
-Variables declared using let and const have block scope.
+Variables declared using let and const have block scope and can't be accessed outside.
 */
 
 if(5 > 3){
@@ -14,7 +14,7 @@ function myFunction() {
     var secret = 1234
 }
 
-console.log(myVar)
+console.log(myVar) // This will work.
 // console.log(myVarTwo) // ReferenceError
 // console.log(myVarThree) // ReferenceError
 // console.log(secret) // Reference Error
