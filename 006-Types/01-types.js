ee4051c05ada423a997e383eb43249a3f68c5204
sushant102004 <sushant.dhiman9812@gmail.com>
2023-02-17 22:51:27 +0530

    Types in JavaScript

---
+++ 006-Types/01-types.js
@@ -0,0 +1,11 @@
+// Primitive types
+console.log(typeof 5)
+console.log(typeof 'Sushant')
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof true)
+
+// Primitive types
+console.log(typeof [])
+console.log(typeof {})
+console.log(typeof function(){})
